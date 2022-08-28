197c09484066f4a06672ea6d3ea4df16122ec4cb
colorful-lollipop <111953102+colorful-lollipop@users.noreply.github.com>
2022-08-28 17:26:59 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ export const config = {
     // 模板消息id
     templateId: "jWP4ZbUjnkqU1K27VSmMtRk5QCMmz8NYGlw6XgwQaL8",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: ["oyX315vTMZXvjAIpez_1QwKN2NVw"],
+    user: ["oyX315vTMZXvjAIpez_1QwKN2NVw","oyX315ryeZfMCuvdtzZ70FebeH40"],
      
     // 信息配置
     // 所在省份
